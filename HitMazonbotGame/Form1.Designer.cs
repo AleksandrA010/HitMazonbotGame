31853d75ee0bf83241ee82ac5c776ab6bb082027
AleksandrA010 <dark_letsplay@mail.ru>
2024-04-17 23:43:04 +0300

    Add project files.

---
+++ HitMazonbotGame/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace HitMazonbotGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonHide = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonExitApp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(50)))));
+            this.buttonExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(50)))));
+            this.buttonExit.FlatAppearance.BorderSize = 0;
+            this.buttonExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(55)))), ((int)(((byte)(60)))));
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.ForeColor = System.Drawing.Color.White;
+            this.buttonExit.Location = new System.Drawing.Point(765, 12);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(50, 50);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "X";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            // 
+            // buttonHide
+            // 
+            this.buttonHide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(50)))));
+            this.buttonHide.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(50)))));
+            this.buttonHide.FlatAppearance.BorderSize = 0;
+            this.buttonHide.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(55)))), ((int)(((byte)(60)))));
+            this.buttonHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHide.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHide.ForeColor = System.Drawing.Color.White;
+            this.buttonHide.Location = new System.Drawing.Point(709, 14);
+            this.buttonHide.Name = "buttonHide";
+            this.buttonHide.Size = new System.Drawing.Size(50, 50);
+            this.buttonHide.TabIndex = 2;
+            this.buttonHide.Text = "–";
+            this.buttonHide.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(370, 51);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(242)))), ((int)(((byte)(248)))));
+            this.buttonPlay.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(50)))));
+            this.buttonPlay.FlatAppearance.BorderSize = 0;
+            this.buttonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlay.Location = new System.Drawing.Point(313, 175);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(200, 46);
+            this.buttonPlay.TabIndex = 4;
+            this.buttonPlay.Text = "Играть";
+            this.buttonPlay.UseVisualStyleBackColor = false;
+            // 
+            // buttonExitApp
+            // 
+            this.buttonExitApp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(242)))), ((int)(((byte)(248)))));
+            this.buttonExitApp.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(50)))));
+            this.buttonExitApp.FlatAppearance.BorderSize = 0;
+            this.buttonExitApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExitApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExitApp.Location = new System.Drawing.Point(313, 245);
+            this.buttonExitApp.Name = "buttonExitApp";
+            this.buttonExitApp.Size = new System.Drawing.Size(200, 46);
+            this.buttonExitApp.TabIndex = 5;
+            this.buttonExitApp.Text = "Выход";
+            this.buttonExitApp.UseVisualStyleBackColor = false;
+            this.buttonExitApp.Click += new System.EventHandler(this.ButtonExitApp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(826, 424);
+            this.Controls.Add(this.buttonExitApp);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonHide);
+            this.Controls.Add(this.buttonExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonHide;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonExitApp;
+    }
+}
+
---
+++ HitMazonbotGame/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HitMazonbotGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.pictureBox1.MouseMove += new MouseEventHandler(this.MouseMoveApp);
+            this.MouseMove += new MouseEventHandler(this.MouseMoveApp);
+            this.buttonExit.MouseClick += new MouseEventHandler(this.ButtonExit_Click);
+            this.buttonHide.MouseClick += new MouseEventHandler(this.ButtonHide_Click);
+        }
+
+        bool MouseClickForm = false;
+        Point startPoint = Point.Empty;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseClickForm = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void MouseMoveApp(object sender, MouseEventArgs e)
+        {
+            if (MouseClickForm)
+            {
+                Point mousePoint = PointToScreen(e.Location);
+                this.Location = new Point(mousePoint.X - startPoint.X, mousePoint.Y - startPoint.Y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseClickForm = false;
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseClickForm = false;
+        }
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseClickForm = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+        
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ButtonHide_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ButtonExitApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
